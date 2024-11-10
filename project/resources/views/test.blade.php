87fc8693ddba4deda436a5f9e67d0803109b3149
KJ-M <rfb0920@gmail.com>
2024-11-10 17:47:01 +0900

    メイン画面のレイアウトが完成しました。

--- project/resources/views/test.blade.php
+++ project/resources/views/test.blade.php
@@ -4,55 +4,56 @@
     <x-slot name="slot" class="">
         <div class="min-h-screen flex flex-col">
             <header class="bg-white dark:bg-gray-400 shadow">
-                <div class="max-w-7xl mx-auto py-1 px-1 sm:px-6 lg:px-8 text-center">
+                <div class="max-w-7xl mx-auto py-1 px-1 sm:px-6 lg:px-8 text-2xl md:text-6xl text-center">
                     SoundSampler
                 </div>
             </header>
             <div class="flex-grow">
                 {{-- sound button --}}
-                <div class="grid grid-cols-2 gap-1 place-items-stretch">
-                    <div class="flex items-center  justify-end mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                <div class="grid grid-cols-2 gap-4 place-items-stretch">
+
+                    <div id="soundBtm-1" class="flex items-center  justify-end mx-4 my-2 md:mt-8 lg:mt-10">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
-                                <p class="text-xl text-center">A</p>
+                                <p class="text-xl text-center">1</p>
                                 <p class="text-center">key</p>
                             </div>
                             <div class="text-center truncate"><Q>sounds->title111111</Q></div>
                         </div>
                     </div>
                     
-                    <div class="flex items-center  justify-start mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                    <div id="soundBtm-2" class="flex items-center  justify-start mx-4 my-2 md:mt-8 lg:mt-10">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
-                                <p class="text-xl text-center">A</p>
+                                <p class="text-xl text-center">2</p>
                                 <p class="text-center">key</p>
                             </div>
                             <div class="text-center truncate"><Q>sounds->title111111</Q></div>
                         </div>
                     </div>
 
-                    <div class="flex items-center  justify-end mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                    <div id="soundBtm-3" class="flex items-center  justify-end mx-4 my-2">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
-                                <p class="text-xl text-center">A</p>
+                                <p class="text-xl text-center">Q</p>
                                 <p class="text-center">key</p>
                             </div>
                             <div class="text-center truncate"><Q>sounds->title111111</Q></div>
                         </div>
                     </div>
                     
-                    <div class="flex items-center  justify-start mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                    <div id="soundBtm-4" class="flex items-center  justify-start mx-4 my-2">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
-                                <p class="text-xl text-center">A</p>
+                                <p class="text-xl text-center">W</p>
                                 <p class="text-center">key</p>
                             </div>
                             <div class="text-center truncate"><Q>sounds->title111111</Q></div>
                         </div>
                     </div>
 
-                    <div class="flex items-center  justify-end mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                    <div id="soundBtm-5" class="flex items-center  justify-end mx-4 my-2">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
                                 <p class="text-xl text-center">A</p>
                                 <p class="text-center">key</p>
@@ -61,30 +62,30 @@
                         </div>
                     </div>
                     
-                    <div class="flex items-center  justify-start mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                    <div id="soundBtm-6" class="flex items-center  justify-start mx-4 my-2">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
-                                <p class="text-xl text-center">A</p>
+                                <p class="text-xl text-center">S</p>
                                 <p class="text-center">key</p>
                             </div>
                             <div class="text-center truncate"><Q>sounds->title111111</Q></div>
                         </div>
                     </div>
 
-                    <div class="flex items-center  justify-end mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                    <div id="soundBtm-7" class="flex items-center  justify-end mx-4 my-2">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
-                                <p class="text-xl text-center">A</p>
+                                <p class="text-xl text-center">Z</p>
                                 <p class="text-center">key</p>
                             </div>
                             <div class="text-center truncate"><Q>sounds->title111111</Q></div>
                         </div>
                     </div>
                     
-                    <div class="flex items-center  justify-start mx-4 my-2">
-                        <div class="w-2/3 h-full bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
+                    <div id="soundBtm-8" class="flex items-center  justify-start mx-4 my-2">
+                        <div class="w-2/3 h-auto md:w-1/2 bg-red-400 border-4 border-gray-50 border-double rounded aspect-square min-w-28 max-w-56">
                             <div class="w-1/3 m-1 mb-3 p-1 bg-gray-300 rounded text-wrap">
-                                <p class="text-xl text-center">A</p>
+                                <p class="text-xl text-center">X</p>
                                 <p class="text-center">key</p>
                             </div>
                             <div class="text-center truncate"><Q>sounds->title111111</Q></div>
@@ -96,19 +97,14 @@
 
             {{-- footer --}}
             <footer class="flex justify-between border p-1 ">
-                <div class="flex-1 flex justify-center" id="sound-btm">
-                    <a href="{{asset('Pasted Graphic 2.png')}}">
-                        <img src="{{ asset('Pasted Graphic 2.png') }}" class="w-16 md:w-24 xl:w-32 h-auto" alt="Description of image">
-                    </a>
-                </div>
-                <div class="flex-1 flex justify-center" id="recording-btm">
+                <div class="flex-1 flex justify-end pr-12 sm:pr-24" id="recording">
                     <a href="{{asset('Pasted Graphic.png')}}">
-                        <img src="{{ asset('Pasted Graphic.png') }}" class="w-16 md:w-24 xl:w-32 h-auto" alt="Description of image">
+                        <img src="{{ asset('Pasted Graphic.png') }}" class="w-14 h-auto md:w-24 " alt="Description of image">
                     </a>
                 </div>
-                <div class="flex-1 flex justify-center" id="file-btm">
-                    <a href="Pasted Graphic 1.png">
-                        <img src="{{ asset('Pasted Graphic 1.png') }}" class="w-16 md:w-24 xl:w-32 h-auto" alt="Description of image">
+                <div class="flex-1 flex justify-start pl-12 sm:pl-24" id="soundData">
+                    <a href="{{asset('Pasted Graphic 2.png')}}">
+                        <img src="{{ asset('Pasted Graphic 2.png') }}" class="w-14 h-auto md:w-24 " alt="Description of image">
                     </a>
                 </div>
             </footer>
